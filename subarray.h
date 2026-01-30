4c9f7205d856154a364ebfed923689fc836e20f7
zaraordyan <103381595+zaraordyan@users.noreply.github.com>
2026-01-30 23:01:20 +0400

    Refactor isSubarray function

--- subarray.h
+++ subarray.h
@@ -1,26 +1,23 @@
 
 // Problem 3
-bool isSubarray(const int arr1[], int m, const int arr2[], int k) {
-    if (m >= k) {
-        for (int i = 0; i <= m - k; i++) {
-            int j = 0;
-            for (; j < k; j++) {
-                if (arr1[i + j] != arr2[j]) {
-                    break;
-                }
+bool firstArrIsSubarray(const int arr1[], int m, const int arr2[], int k) {
+    for (int i = 0; i <= k - m; i++) {
+        int j = 0;
+        for (; j < m; j++) {
+            if (arr2[i + j] != arr1[j]) {
+                break;
             }
-            if (j == k) return true;
         }
-    } else {
-        for (int i = 0; i <= k - m; i++) {
-            int j = 0;
-            for (; j < m; j++) {
-                if (arr2[i + j] != arr1[j]) {
-                    break;
-                }
-            }
-            if (j == m) return true;
-        }
+        if (j == m) return true;
     }
     return false;
+}
+
+bool isSubarray(const int arr1[], int m, const int arr2[], int k) {
+    
+    if (m >= k) {
+        return firstArrIsSubarray(arr2, k, arr1, m);
+    } else {
+        return firstArrIsSubarray(arr1, m, arr2, k);
+    }
 }
